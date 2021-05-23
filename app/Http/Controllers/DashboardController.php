40cce15b063d3b73002672dcfbbf3adb5aedc3f6
pippo-github <63654824+pippo-github@users.noreply.github.com>
2021-05-23 18:05:42 +0000

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -28,33 +28,10 @@ class DashboardController extends Controller
      */
     public function index()
     {
-
-        // punta alla vista: "dashboard", scelta da me
-        // prima puntava alla view home di default
-
-
-        // modo classico mostra tutti i records
-
-        // $tabella = new Posts();   
-        // $records = $tabella::all();
-        
-        // return view('dashboard')->with("recordsPerLaView", $records);
-
-
-        // mostra solo i record legati all'utente loggato
-        // necessita delle relazioni uno-a-molti fra le tabelle:
-        // user ====many====> posts
-        
-
+       
         $userID = auth()->user()->id;
         $utente = User::find($userID);
 
-
-        // estrae nome dell'utente loggato col relativo ID";
-        // tramite relazione HasMany";
-
         return view('dashboard')->with("recordsPerLaView", $utente->posts);
-
-
     }
 }
